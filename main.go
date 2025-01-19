cc8418a3ac6978533388c8dad99b16c5064187c8
JustDrven <petr.nemec11111@gmail.com>
2025-01-19 17:43:55 +0100

    Added middle ware into handleFunc

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"cacher/router"
+	"cacher/security"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,10 +10,10 @@ import (
 
 func main() {
 
-	http.HandleFunc("/v1/valid", router.IsValid)
-	http.HandleFunc("/v1/get", router.GetData)
-	http.HandleFunc("/v1/set", router.SaveData)
-	http.HandleFunc("/v1/remove", router.RemoveData)
+	http.HandleFunc("/v1/valid", security.AuthMiddleware(router.IsValid))
+	http.HandleFunc("/v1/get", security.AuthMiddleware(router.GetData))
+	http.HandleFunc("/v1/set", security.AuthMiddleware(router.SaveData))
+	http.HandleFunc("/v1/remove", security.AuthMiddleware(router.RemoveData))
 
 	fmt.Println("The server is starting..")
 	log.Fatal(http.ListenAndServe(":8080", nil))
