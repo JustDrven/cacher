a7e133bdcc263fb4542bb35030d1d2d7e3d037c2
JustDrven <petr.nemec11111@gmail.com>
2025-01-19 18:08:28 +0100

    Change printer from "fmt" to "log"

--- main.go
+++ main.go
@@ -3,18 +3,16 @@ package main
 import (
 	"cacher/api"
 	"cacher/security"
-	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
-
 	http.HandleFunc("/v1/valid", security.AuthMiddleware(api.IsValid))
 	http.HandleFunc("/v1/get", security.AuthMiddleware(api.GetData))
 	http.HandleFunc("/v1/set", security.AuthMiddleware(api.SaveData))
 	http.HandleFunc("/v1/remove", security.AuthMiddleware(api.RemoveData))
 
-	fmt.Println("The server is starting..")
+	log.Println("The server is starting..")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
