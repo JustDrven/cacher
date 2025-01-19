b27c3e923d19cf7be16ee3e67c88f11ba8943659
JustDrven <petr.nemec11111@gmail.com>
2025-01-19 22:23:22 +0100

    Updated types of endpoints

--- api/api.go
+++ api/api.go
@@ -11,10 +11,10 @@ type App struct {
 }
 
 func StartAPI(app App) {
-	http.HandleFunc("/v1/valid", security.AuthMiddleware(IsValid))
-	http.HandleFunc("/v1/get", security.AuthMiddleware(GetData))
-	http.HandleFunc("/v1/set", security.AuthMiddleware(SaveData))
-	http.HandleFunc("/v1/remove", security.AuthMiddleware(RemoveData))
+	http.HandleFunc("GET /v1/valid", security.AuthMiddleware(IsValid))
+	http.HandleFunc("GET /v1/get", security.AuthMiddleware(GetData))
+	http.HandleFunc("POST /v1/set", security.AuthMiddleware(SaveData))
+	http.HandleFunc("DELETE /v1/remove", security.AuthMiddleware(RemoveData))
 
 	log.Println("The server is starting at " + app.Addr + "..")
 	log.Fatal(http.ListenAndServe(app.Addr, nil))
