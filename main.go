29f3ffd562157e631e864024982fa1418a8030fe
JustDrven <petr.nemec11111@gmail.com>
2025-01-19 18:04:54 +0100

    Updated folder from "router" to "api"

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"cacher/router"
+	"cacher/api"
 	"cacher/security"
 	"fmt"
 	"log"
@@ -10,10 +10,10 @@ import (
 
 func main() {
 
-	http.HandleFunc("/v1/valid", security.AuthMiddleware(router.IsValid))
-	http.HandleFunc("/v1/get", security.AuthMiddleware(router.GetData))
-	http.HandleFunc("/v1/set", security.AuthMiddleware(router.SaveData))
-	http.HandleFunc("/v1/remove", security.AuthMiddleware(router.RemoveData))
+	http.HandleFunc("/v1/valid", security.AuthMiddleware(api.IsValid))
+	http.HandleFunc("/v1/get", security.AuthMiddleware(api.GetData))
+	http.HandleFunc("/v1/set", security.AuthMiddleware(api.SaveData))
+	http.HandleFunc("/v1/remove", security.AuthMiddleware(api.RemoveData))
 
 	fmt.Println("The server is starting..")
 	log.Fatal(http.ListenAndServe(":8080", nil))
