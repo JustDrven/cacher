7b9746686e624e4abac9f6d5d9179630f7ed4d60
JustDrven <petr.nemec11111@gmail.com>
2025-01-19 17:44:20 +0100

    Created file manager for .env file

---
+++ file/env.go
@@ -0,0 +1,43 @@
+package file
+
+import (
+	"bufio"
+	"os"
+	"strings"
+)
+
+func GetEnv(key string) string {
+	file, err := os.Open(".env")
+	if err != nil {
+		return ""
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		line = strings.TrimSpace(line)
+		if line == "" || strings.HasPrefix(line, "#") {
+			continue
+		}
+
+		parts := strings.SplitN(line, "=", 2)
+		if len(parts) != 2 {
+			continue
+		}
+
+		currentKey := strings.TrimSpace(parts[0])
+		value := strings.TrimSpace(parts[1])
+
+		if currentKey == key {
+			return value
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return ""
+	}
+
+	return ""
+}
