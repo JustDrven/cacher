aa92d4817fa4b615e60360fdfde1b02dda69cedb
JustDrven <petr.nemec11111@gmail.com>
2025-01-19 17:42:12 +0100

    Added middle ware

--- security/security.go
+++ security/security.go
@@ -1,13 +1,34 @@
 package security
 
 import (
+	"cacher/data"
+	"cacher/file"
+	"encoding/json"
 	"net/http"
 	"os"
 )
 
-func CheckAuthorization(header http.Header) bool {
-	if header.Get("X-API-Key") == os.Getenv("API_KEY") {
-		return false
+func AuthMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
+		var API_KEY string = ""
+		if file.GetEnv("DEV_API_KEY") != "" {
+			API_KEY = file.GetEnv("DEV_API_KEY")
+		} else {
+			API_KEY = os.Getenv("API_KEY")
+		}
+
+		if r.Header.Get("X-API-Key") != API_KEY {
+			w.WriteHeader(http.StatusUnauthorized)
+
+			json.NewEncoder(w).Encode(data.ErrorResponse{
+				Error:   401,
+				Message: "Unauthorized",
+			})
+
+			return
+		}
+		next(w, r)
 	}
-	return true
 }
