6909490c21359936fd9406112bd89a4aefb5bd2f
JustDrven <petr.nemec11111@gmail.com>
2025-01-20 22:46:38 +0100

    Added own "404 - not found page" for microservice design

--- api/api.go
+++ api/api.go
@@ -8,10 +8,13 @@ import (
 )
 
 func StartAPI(app app.Application) {
+
 	http.HandleFunc("GET /v1/valid", security.AuthMiddleware(IsValid))
 	http.HandleFunc("GET /v1/get", security.AuthMiddleware(GetData))
 	http.HandleFunc("POST /v1/set", security.AuthMiddleware(SaveData))
 	http.HandleFunc("DELETE /v1/remove", security.AuthMiddleware(RemoveData))
+
+	http.HandleFunc("/", NotFound)
 
 	log.Println("The server is starting at http://localhost" + app.Config.Addr + "..")
 	log.Fatal(http.ListenAndServe(app.Config.Addr, nil))
