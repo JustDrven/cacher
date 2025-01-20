e4640b54ca1704896caa2dc5b74009e4fef3afb6
JustDrven <petr.nemec11111@gmail.com>
2025-01-20 22:31:08 +0100

    Created settings  file for application

---
+++ app/settings/settings.go
@@ -0,0 +1,13 @@
+package settings
+
+import (
+	v1 "cacher/app"
+	"cacher/file"
+)
+
+var App = v1.Application{
+	Debug: file.GetEnvToBool("DEBUG"),
+	Config: v1.ApplicationConfig{
+		Addr: ":8080",
+	},
+}
