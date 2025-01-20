9ec6b656e2e99c794c6e8535551620c2f9391bb9
JustDrven <petr.nemec11111@gmail.com>
2025-01-20 22:31:56 +0100

    Changed from application to application settings

--- api/api.go
+++ api/api.go
@@ -1,21 +1,18 @@
 package api
 
 import (
+	"cacher/app"
 	"cacher/security"
 	"log"
 	"net/http"
 )
 
-type App struct {
-	Addr string
-}
-
-func StartAPI(app App) {
+func StartAPI(app app.Application) {
 	http.HandleFunc("GET /v1/valid", security.AuthMiddleware(IsValid))
 	http.HandleFunc("GET /v1/get", security.AuthMiddleware(GetData))
 	http.HandleFunc("POST /v1/set", security.AuthMiddleware(SaveData))
 	http.HandleFunc("DELETE /v1/remove", security.AuthMiddleware(RemoveData))
 
-	log.Println("The server is starting at " + app.Addr + "..")
-	log.Fatal(http.ListenAndServe(app.Addr, nil))
+	log.Println("The server is starting at http://localhost" + app.Config.Addr + "..")
+	log.Fatal(http.ListenAndServe(app.Config.Addr, nil))
 }
