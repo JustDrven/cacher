e546e558a36dd76bddeb2e26633083cb37187ff5
JustDrven <petr.nemec11111@gmail.com>
2025-01-23 23:31:48 +0100

    Applied files manager

--- api/api.go
+++ api/api.go
@@ -5,9 +5,11 @@ import (
 	"cacher/manager"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func StartAPI(app app.Application) {
+	manager.CheckFiles()
 
 	manager.RegisterRouter("GET", "valid", IsValid, true)
 	manager.RegisterRouter("GET", "get", GetData, true)
@@ -16,5 +18,7 @@ func StartAPI(app app.Application) {
 	manager.RegisterRouter("POST", "replace", ReplaceData, true)
 
 	log.Println("The server is starting at http://localhost" + app.Config.Addr + "..")
+	log.Println("Debug is " + strconv.FormatBool(app.Debug))
+
 	log.Fatal(http.ListenAndServe(app.Config.Addr, nil))
 }
