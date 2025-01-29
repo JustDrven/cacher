9cb5e1abe3b41b5adc1ffcc0f6473cf501806d3a
JustDrven <petr.nemec11111@gmail.com>
2025-01-29 17:27:14 +0100

    Improved api key security initialization and modified code for better readability

--- security/security.go
+++ security/security.go
@@ -1,25 +1,34 @@
 package security
 
 import (
+	"cacher/app/settings"
 	"cacher/data"
 	"cacher/file"
 	"encoding/json"
+	"log"
 	"net/http"
 	"os"
 )
+
+var API_KEY string = ""
+
+func init() {
+	if file.GetEnv("DEV_API_KEY") != "" {
+		API_KEY = file.GetEnv("DEV_API_KEY")
+	} else {
+		API_KEY = os.Getenv("API_KEY")
+	}
+
+	if settings.App.Debug {
+		log.Println("Security has been successfully initialized")
+	}
+}
 
 func AuthMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 
-		var API_KEY string = ""
-		if file.GetEnv("DEV_API_KEY") != "" {
-			API_KEY = file.GetEnv("DEV_API_KEY")
-		} else {
-			API_KEY = os.Getenv("API_KEY")
-		}
-
-		if r.Header.Get("X-API-Key") != API_KEY {
+		if !IsAPIKeyValid(r) {
 			w.WriteHeader(http.StatusUnauthorized)
 
 			json.NewEncoder(w).Encode(data.ErrorResponse{
@@ -31,4 +40,8 @@ func AuthMiddleware(next http.HandlerFunc) http.HandlerFunc {
 		}
 		next(w, r)
 	}
+}
+
+func IsAPIKeyValid(r *http.Request) bool {
+	return r.Header.Get("X-API-Key") == API_KEY
 }
