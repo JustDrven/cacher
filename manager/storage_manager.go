95d9a6f1b37a273c1b3c3a97ec0522ff816c1ed3
JustDrven <petr.nemec11111@gmail.com>
2025-02-04 22:41:23 +0100

    feat: created storage manager for clean code

---
+++ manager/storage_manager.go
@@ -0,0 +1,32 @@
+package manager
+
+import (
+	"cacher/utility"
+	"os"
+)
+
+const PREFIX string = utility.SOURCE
+
+func Exist(key string) bool {
+	return os.Getenv(PREFIX+key) != ""
+}
+
+func Set(key string, value string) {
+	os.Setenv(PREFIX+key, value)
+}
+
+func Remove(key string) {
+	if !Exist(key) {
+		return
+	}
+
+	os.Unsetenv(PREFIX + key)
+}
+
+func Get(key string) (string, bool) {
+	if !Exist(key) {
+		return "", true
+	}
+
+	return os.Getenv(PREFIX + key), false
+}
