212603d8e16e3b37f34f38d4fd840e22073bb726
JustDrven <petr.nemec11111@gmail.com>
2025-02-04 23:42:40 +0100

    feat: added api port into createFile.sh

--- cmd/createFile.sh
+++ cmd/createFile.sh
@@ -6,6 +6,7 @@ if ! [ -f $file ]; then
     touch $file
     echo "DEV_API_KEY=test" >> $file;
     echo "DEBUG=true" >> $file;
+    echo "API_PORT=8080" >> $file;
 
     echo ""
     echo "The data file was created"
